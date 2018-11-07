e98d945aa428bd463b9d9e704d8334bc40a25ac4
Rafa <rafa@rafadev.com>
2018-11-07 15:44:52 -0800

    Create splitFile method and test

--- __tests__/services/scores-services.test.js
+++ __tests__/services/scores-services.test.js
@@ -1,17 +1,37 @@
-const { readFile } = require('../../services/scores-services');
+const { readFile, splitFile } = require('../../services/scores-services');
 
 /******************************* Test readFile ********************************/
 test('Reads from .txt file and returns its contents as a string', async () => {
   const returnedString = await readFile(
     '__tests__/__test-inputs__/test-input.txt'
   );
-  //prettier-ignore
+
   const expectedString =
     'Sounders 3, Chelsea 2' + '\n' +
     'Liverpool 11, Galexy 3' + '\n' +
-		'Manchester 3, D.C. United 3' + '\n' +
+    'Manchester 3, D.C. United 3' + '\n' +
     'D.C. United 3, Atlanta United 25' + '\n' +
-		'Real Salt Lake 1, Sounders 2';
+    'Real Salt Lake 1, Sounders 2';
 
   expect(returnedString).toEqual(expectedString);
+});
+
+/******************************* Test splitFile *******************************/
+test('Creates properly formatted 2d array from input string', () => {
+  const inputString =
+    `Sounders 3, Chelsea 2
+    Liverpool 11, Galexy 3
+		Manchester 3, D.C. United 3
+    D.C. United 3, Atlanta United 25
+    Real Salt Lake 1, Sounders 2`;
+
+  const expectedArray = [
+    ['Sounders', '3', 'Chelsea', '2'],
+    ['Liverpool', '11', 'Galexy', '3'],
+    ['Manchester', '3', 'D.C. United', '3'],
+    ['D.C. United', '3', 'Atlanta United', '25'],
+    ['Real Salt Lake', '1', 'Sounders', '2']
+  ];
+
+  expect(splitFile(inputString)).toEqual(expectedArray);
 });
--- services/scores-services.js
+++ services/scores-services.js
@@ -15,4 +15,19 @@ const readFile = async filePath => {
   }
 };
 
-module.exports = { readFile };
+/******* Split string to 2d array [[team1, score1, team2, score2],...] ********/
+const splitFile = input => {
+  return input.split('\n').map(game => {
+    const splitGame = game
+      .replace(',', '')
+      .split(/(\d+)/)
+      .filter(Boolean);
+    splitGame[0] = splitGame[0].trim();
+    splitGame[2] = splitGame[2].trim();
+    return splitGame;
+  });
+};
+
+
+
+module.exports = { readFile, splitFile };
